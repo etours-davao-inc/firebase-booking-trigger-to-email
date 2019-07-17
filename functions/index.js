a811a81284b18102d3a77323f2f19ae7554e3dc6
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-07-17 13:35:28 +0800

    Refactor make use of promises

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 const nunjucks = require('nunjucks');
 
-exports.sendEmailToStaff = functions.firestore
+exports.emailBookingRequestToStaff = functions.firestore
   .document(functions.config().db.collection)
   .onCreate((snapshot, context) => {
     const newBookingRequest = snapshot.data()
@@ -16,32 +16,42 @@ exports.sendEmailToStaff = functions.firestore
         pass: functions.config().gmail.password
       } 
     })
-    
-    console.log('Function sendEmailToStaff starting...');
 
-    nunjucks.configure({ autoescape: true });
-
-    const payload = nunjucks.render('./templates/booking.html', newBookingRequest)
-    
     const mailSettings = {
       from: `Tom Claudio <${functions.config().gmail.email}>`,
       to: functions.config().staff.email,
-      subject: `Booking request for ${newBookingRequest.info.code}`,
-      text: `Booking request for ${newBookingRequest.info.code}`,
+      subject: `Booking request for ${newBookingRequest.package.code}`,
+      text: `Booking request for ${newBookingRequest.package.code}`,
       html: payload
     }
+    
+    console.log('Service emailBookingRequestToStaff starting...');
+
+    nunjucks.configure({ autoescape: true });
+
+    console.log("Rendering booking information payload to template...")
+
+    nunjucks.render('./templates/booking.html', newBookingRequest)
+      .then((payload) => {
+        console.log("Render Successful...")
+        mailSettings.html = payload
+        return true;
+      })
+      .catch(error => {
+        console.log("Error rendering payload")
+        return null;
+      })
+
+    console.log(`Sending booking request to ${functions.config().staff.email}...`)
 
     mailTransport.sendMail(mailSettings)
       .then(() => {
-        console.log("Email now sending...")
-      })
-      .then(() => {
-        console.log("Email succesfully sent")
-      })
-      .then(() => { 
-        return true 
+        console.log("Email succesfully sent");
+        return true;
+
       })
       .catch (error => {
         console.log("Error sending mail", error)
+        return null;
       }) 
   })
