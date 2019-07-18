0601718af0f23655692dc5d64037a5d152a4c7a2
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-07-18 11:27:37 +0800

    Refactor to conform to clean code :-)

--- functions/index.js
+++ functions/index.js
@@ -5,59 +5,81 @@ const nunjucks = require('nunjucks');
 exports.emailBookingRequestToStaff = functions.firestore
   .document(functions.config().db.collection)
   .onCreate((snapshot, context) => {
-    const newBookingRequest = snapshot.data()
-    newBookingRequest.input.inquiryDate = newBookingRequest.input.inquiryDate.toDate();
-    console.log(newBookingRequest)
+    var bookingData = snapshot.data()
+    console.log(`Booking request is for ${bookingData.package.type}...`, bookingData)
 
-    const type = newBookingRequest.package.type
-    console.log(`Booking request is for ${type}.`)
-    let template;
-    if (type === "multiday") {
-      template = "multiday.html"
-      newBookingRequest.input.tourDates.from = newBookingRequest.input.tourDates.from.toDate()
-      newBookingRequest.input.tourDates.to = newBookingRequest.input.tourDates.to.toDate()
-      
-    } else {
-      template = "daytour.html"
-      newBookingRequest.input.tourDate = newBookingRequest.input.tourDate.toDate();
-    }
-    console.log(`Template to use is ${template}...`)
+    bookingData = convertTimeStampToJSDate(bookingData);
+    let emailTemplate = determineEmailTemplate(bookingData);
+    let payload = renderEmailPayload(bookingData, emailTemplate);
+    const mailTransport = prepareMailTransport();
+    const mailSettings = prepareMailSettings(bookingData, payload);
 
-    const mailTransport = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: functions.config().gmail.email,
-        pass: functions.config().gmail.password
-      } 
-    })
-
-    const mailSettings = {
-      from: `Tom Claudio <${functions.config().gmail.email}>`,
-      to: functions.config().staff.email,
-      subject: `Booking request for ${newBookingRequest.package.code}`,
-      text: `Booking request for ${newBookingRequest.package.code}`,
-    }
-    
-    console.log('Service emailBookingRequestToStaff starting...');
-
-    nunjucks.configure({ autoescape: true });
-
-    console.log("Rendering booking information payload to template...")
-    
-    const payload = nunjucks.render(`./templates/${template}`, newBookingRequest);
-
-    mailSettings.html = payload;
-
-    console.log(`Sending booking request to ${functions.config().staff.email}...`)
-
-    mailTransport.sendMail(mailSettings)
-      .then(() => {
-        console.log("Email succesfully sent");
-        return true;
-
-      })
-      .catch (error => {
-        console.log("Error sending mail", error)
-        return null;
-      }) 
+    sendEmail(mailTransport, mailSettings);
   })
+
+const convertTimeStampToJSDate = function (bookingData) {
+  console.log(`Converting Timestamp Objects to JS Dates...`)
+  bookingData.input.inquiryDate = bookingData.input.inquiryDate.toDate();
+
+  if (bookingData.package.type === "multiday") {
+    bookingData.input.tourDates.from = bookingData.input.tourDates.from.toDate()
+    bookingData.input.tourDates.to = bookingData.input.tourDates.to.toDate()
+
+  } else {
+    bookingData.input.tourDate = bookingData.input.tourDate.toDate();
+  }
+  return bookingData;
+}
+
+const determineEmailTemplate = function (bookingData) {
+  let template;
+  if (bookingData.package.type === "multiday") {
+    template = "multiday.html"
+  } else {
+    template = "daytour.html"
+  }
+  console.log(`Template to use is ${template}...`)
+  return `./templates/${template}`
+}
+
+const renderEmailPayload = function (bookingData, emailTemplate) {
+  nunjucks.configure({ autoescape: true });
+  console.log("Rendering booking data to email template...")
+  return nunjucks.render(emailTemplate, bookingData);
+}
+
+const prepareMailTransport = function () {
+  console.log("Preparing mail transport...")
+  return nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: functions.config().gmail.email,
+      pass: functions.config().gmail.password
+    }
+  })
+}
+
+const prepareMailSettings = function (bookingData, payload) {
+  console.log("Preparing mail settings...")
+  return {
+    from: `Tom Claudio <${functions.config().gmail.email}>`,
+    to: functions.config().staff.email,
+    subject: `Booking request for ${bookingData.package.code}`,
+    text: `Booking request for ${bookingData.package.code}`,
+    html: payload
+  }
+}
+
+const sendEmail = function (mailTransport, mailSettings) {
+  console.log(`Sending booking request to ${functions.config().staff.email}...`)
+  mailTransport.sendMail(mailSettings)
+    .then(() => {
+      console.log("Email succesfully sent");
+      return true;
+
+    })
+    .catch(error => {
+      console.log("Error sending mail", error)
+      return null;
+    })
+}
